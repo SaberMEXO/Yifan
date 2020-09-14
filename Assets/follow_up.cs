dda40ddb5f7f61f2e4c99ec1fbae07d0c8005803
Yifan <ym2159@nyu.edu>
2020-09-14 07:16:41 -0700

    first commit

---
+++ Assets/follow_up.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class follow_up : MonoBehaviour
+{
+    public Transform characterPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(characterPlayer.position.x, characterPlayer.position.y, transform.position.z);
+    }
+}
